e169afc68c3628448c59de9994e97f1573ce780d
Thiago <thyagodelimamachado@gmail.com>
2026-04-01 23:52:22 -0300

    01/04 - Inicialização Spring Boot

---
+++ src/main/java/br/com/thiago/spring_boot_essentials/SpringBootEssentialsApplication.java
@@ -0,0 +1,13 @@
+package br.com.thiago.spring_boot_essentials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEssentialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEssentialsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/thiago/spring_boot_essentials/SpringBootEssentialsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thiago.spring_boot_essentials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEssentialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
